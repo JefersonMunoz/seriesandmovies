ef93e4e56866f5f81ec993f17d7a6dc05ef03e30
Jeferson Estiven <jeferson.posada@hotmail.com>
2025-09-15 01:38:48 -0500

    new updates

--- lib_dominio/Nucleo/DatosGenerales.cs
+++ lib_dominio/Nucleo/DatosGenerales.cs
@@ -2,7 +2,7 @@
 {
     public class DatosGenerales
     {
-        public static string ruta_json = @"C:\Users\AA\Desktop\hoteles\config.abc";
+        public static string ruta_json = @"C:\Users\jefer\Documents\ITM\Programacion-de-software\Repos\seriesandmovies\config.abc";
         public static bool usa_azure = false;
         public static string clave = "EVBgi345936456ghhVBJGtgnifytsidi3456678jhgUTytutyiiyi";
         public static string usuario_datos = EncriptarConversor.Encriptar("Test.Trghhjsgdj");
