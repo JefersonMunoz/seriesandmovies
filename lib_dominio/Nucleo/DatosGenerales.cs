28d9121871232cd7fa356cf55896953530886af2
Caterineperea <caterineperea1124101@correo.itm.edu.co>
2025-09-16 00:32:22 -0500

    Pruebas

--- lib_dominio/Nucleo/DatosGenerales.cs
+++ lib_dominio/Nucleo/DatosGenerales.cs
@@ -2,7 +2,8 @@
 {
     public class DatosGenerales
     {
-        public static string ruta_json = @"C:\Users\jefer\Documents\ITM\Programacion-de-software\Repos\seriesandmovies\config.abc";
+        public static string ruta_json = @"C:\Universidad\ITM\5 semestre\Programación de software\seriesandmovies\config.abc";
+        //public static string ruta_json = @"C:\Users\jefer\Documents\ITM\Programacion-de-software\Repos\seriesandmovies\config.abc";
         public static bool usa_azure = false;
         public static string clave = "EVBgi345936456ghhVBJGtgnifytsidi3456678jhgUTytutyiiyi";
         public static string usuario_datos = EncriptarConversor.Encriptar("Test.Trghhjsgdj");
